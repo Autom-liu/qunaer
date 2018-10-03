caf7235b51736d30ba273e19d88c82263dddd5d7
liusimin <liusimin@tungee.com>
2018-10-03 21:49:25 +0800

    引入stylus变量

--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="header">
 		<div class="header-left">
-			<i class="iconfont">&#xe624;</i>
+			<i class="iconfont icon-back">&#xe624;</i>
 		</div>
 		<div class="header-input">
-			<i class="iconfont">&#xe632;</i>
+			<i class="iconfont icon-serach">&#xe632;</i>
 			输入城市/景点
 		</div>
 		<div class="header-right">
 			城市
-			<i class="iconfont">&#xe62d;</i>
+			<i class="iconfont icon-arrow">&#xe62d;</i>
 		</div>
 	</div>
 </template>
@@ -21,22 +21,26 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '../../../assets/style/varibles.styl';
 .header
 	display: flex
 	height: 0.86rem
 	line-height: .86rem
-	background-color: #00bcd4
+	background-color: $bgColor
 	color: #fff
 	.header-left
 		float: left
 		width: 0.64rem
 		text-align: center
+		.icon-back
+			font-size: .4rem
 	.header-input
 		flex: 1
 		height: .64rem
 		line-height: .64rem
 		margin-top: .12rem
 		margin-left: .2rem
+		padding-left: .2rem
 		background-color #fff
 		border-radius: 5px
 		color: #ccc;

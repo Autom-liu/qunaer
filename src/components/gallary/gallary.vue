f7da4e2d72b028adaae6b5b2752c944905137468
liusimin <liusimin@tungee.com>
2018-10-04 20:34:26 +0800

    公共画廊组件

---
+++ src/components/gallary/gallary.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="container">
+		<div class="wrapper">
+			<swiper :options="swiperOption">
+				<swiper-slide v-for="item of imgs" :key="item.id">
+					<img class="swiper-img" :src="item.imgUrl" alt="">
+				</swiper-slide>
+				<!-- Optional controls -->
+				<div class="swiper-pagination"  slot="pagination"></div>
+			</swiper>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'gallary',
+	props: {
+		imgs: {
+			type: Array,
+			default() {
+				return [];
+			},
+		},
+	},
+	data() {
+		return {
+			swiperOption: {
+				pagination: '.swiper-pagination',
+				paginationType: 'fraction',
+				autoplay: false,
+				observeParents: true,
+				observer: true,
+			},
+		};
+	},
+};
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-container
+		overflow inherit
+
+	.container
+		display flex
+		flex-direction column
+		justify-content center
+		position fixed
+		top 0
+		left 0
+		right 0
+		bottom 0
+		z-index 99
+		background-color rgba(0,0,0,0.8)
+		.wrapper
+			height 0
+			width 100%
+			padding-bottom 100%
+			.swiper-img
+				width 100%
+			.swiper-pagination
+				color #fff;
+				bottom -1rem
+</style>
+
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,15 +1,40 @@
 <template>
 	<div class="wrapper">
-		<detail-banner></detail-banner>
+		<detail-banner @click.native="bannerClick"></detail-banner>
+		<gallary :imgs="imgs" v-show="showGallary" @click.native="bannerClick"></gallary>
 	</div>
 </template>
 
 <script>
+import gallary from '@/components/gallary/gallary';
 import detailBanner from './components/banner';
 
 export default {
 	name: 'detail',
-	components: { detailBanner },
+	components: {
+		detailBanner,
+		gallary,
+	},
+	data() {
+		return {
+			imgs: [
+				{
+					id: 1,
+					imgUrl: 'http://img1.qunarzz.com/wugc/p123/201211/19/a2045d091f02b25493835fbb.png_r_800x800_c59f0eba.png',
+				},
+				{
+					id: 2,
+					imgUrl: 'http://img1.qunarzz.com/sight/p0/1410/e3/73da8d3e19cdc41c1932d4fcd22ec792.water.jpg_r_800x800_7f96ccf9.jpg',
+				},
+			],
+			showGallary: false,
+		};
+	},
+	methods: {
+		bannerClick() {
+			this.showGallary = !this.showGallary;
+		},
+	},
 };
 </script>
 

f4e9f6b9017c08facf47749cedbc0e5717d7b250
liusimin <liusimin@tungee.com>
2018-10-04 19:14:22 +0800

    详情页路由

---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="wrapper">
+		<img class="banner-img" src="" alt="">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'detail',
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
